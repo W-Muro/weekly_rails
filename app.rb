908dce4ddc45bf1968d2dcaee2354e388f9a5b6e
Muro <muro3690.lidlil@gmail.com>
2020-08-24 10:24:49 +0900

    スクレイピング 部分を実装完了

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
-require 'bundler/setup'
-Bundler.require
-require 'sinatra/reloader' if development?
+# require 'bundler/setup'
+# Bundler.require
+# require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'nokogiri'
+require "date"
+
+url = "https://techracho.bpsinc.jp/category/ruby-rails-related"
+
+page = Nokogiri::HTML(URI.open(url))
+
+result = page.xpath("//time[@class='article-card-date']/text()")[0]
+result = Time.parse(result).to_date
+
+# 日付の取得
+date = Date.today
+
+if result > date
+  p "ブログが更新されています。"
+else
+  p "本日の更新はありません。"
+end
