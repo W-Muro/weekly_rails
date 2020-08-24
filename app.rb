3a07b8350797c5b792a6fc237768a185e07c19ee
Muro <muro3690.lidlil@gmail.com>
2020-08-24 11:28:09 +0900

    save

--- app.rb
+++ app.rb
@@ -30,14 +30,6 @@ def client
   }
 end
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_id = ENV["LINE_CHANNEL_ID"]
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
-
 post '/callback' do
   body = request.body.read
 
